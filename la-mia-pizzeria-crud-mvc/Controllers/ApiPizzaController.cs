8714e8790d8265598683d0683e60b54c1ba4fd92
Valerio Bartoletti <valerio.bartoletti97@gmail.com>
2024-05-24 16:08:28 +0200

    added web api application home

---
+++ la-mia-pizzeria-crud-mvc/Controllers/ApiPizzaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_crud_mvc.Controllers
+{
+    public class ApiPizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- la-mia-pizzeria-crud-mvc/Program.cs
+++ la-mia-pizzeria-crud-mvc/Program.cs
@@ -33,7 +33,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Pizza}/{action=Index}/{id?}");
+    pattern: "{controller=ApiPizza}/{action=Index}/{id?}");
 app.MapRazorPages();
 PizzaManager.Seed();
 app.Run();
